eb29a9a44973ef2d0da2f4dcd90ba6ae7f502786
ShamNeko <mkdvlp01@gmail.com>
2025-01-09 22:03:12 +0900

    外部からテキストを操作する

--- Assets/PlayerHP.cs
+++ Assets/PlayerHP.cs
@@ -3,17 +3,10 @@ using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject hpGameObject;
     void Start()
     {
-        Text HPText = GetComponent<Text>();
-        HPText.text = "100";
-        HPText.color = new Color(1f, 0f, 0f/1f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Text text = hpGameObject.GetComponent<Text>();
+        text.text = "50";
     }
 }
